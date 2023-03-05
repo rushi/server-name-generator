37b5041c1fe457522e3f02f3b353318e28e95528
Rushi Vishavadia <rushi@xola.com>
2023-03-05 11:42:38 +0530

    Remove spaces and lowercase

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ const getName = (seed, type, separatorChar, style, length = DEFAULT_LENGTH) => {
     });
 
     // return deburr(shortName.replace(/(&)+/gi, "-").toLowerCase());
-    return deburr(shortName);
+    return deburr(shortName).replace(" ", "-").toLowerCase();
 };
 
 module.exports = app;
